629aec0cf929bb7b1ea4b0ed51676ce063f0fc6c
cristianvarela1 <16954354-cristianvarela1@users.noreply.replit.com>
2023-09-06 12:45:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print (" Cris")
+print ("Domingo , 25 de septiembre de 2022")
+print (" i am singing up for Replit´s 100 days of Phyton callenge!")
+print ("""
+I will make sure to spend some time every day coding along , for a minimumm of 10 minutes of day
+
+I´ll BE USING Replit, an amazing online IDE so I can do this from my phone wherever I happen to be.
+
+No excuses for not coding from the middle of a fleld!
+
+I am feeling amaizing""")
+print ("You can follow my progress at replit.com/@cristianvarela1")
